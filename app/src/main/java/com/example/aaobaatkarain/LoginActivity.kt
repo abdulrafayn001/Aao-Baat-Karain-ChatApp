38b6b5687edc1c4944e4948372f25a3ce1a58e84
Moiz Asad <moizasad86@gmail.com>
2020-12-28 07:56:42 +0500

    user already logedin Validation

--- app/src/main/java/com/example/aaobaatkarain/LoginActivity.kt
+++ app/src/main/java/com/example/aaobaatkarain/LoginActivity.kt
@@ -1,14 +1,27 @@
 package com.example.aaobaatkarain
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
+
+    var fbUser:FirebaseUser? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
 
-
-
+    override fun onStart() {
+        super.onStart()
+        fbUser = FirebaseAuth.getInstance().currentUser
+        if(fbUser != null)
+        {
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
     }
 }
