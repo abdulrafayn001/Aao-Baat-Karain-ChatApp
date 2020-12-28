46a343190f9b15d8d0a7d122ff7e67a156ca620e
Abdulrafay <abdulrafayn001@gmail.com>
2020-12-29 04:08:50 +0500

    Add UserAdapter class (for list in search fragment) and retrive all users in searchh fragment

--- app/src/main/java/com/example/aaobaatkarain/AdapterClasses/UserAdapter.kt
+++ app/src/main/java/com/example/aaobaatkarain/AdapterClasses/UserAdapter.kt
@@ -1,5 +1,60 @@
 package com.example.aaobaatkarain.AdapterClasses
 
-class UserAdapter {
-    
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.aaobaatkarain.ModelClasses.Users
+import com.example.aaobaatkarain.R
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
+
+class UserAdapter (context: Context,users:List<Users>,isChatChecked:Boolean): RecyclerView.Adapter<UserAdapter.ViewHolder?>() {
+    private  val context:Context
+    private  val users:List<Users>
+    private  var isChatChecked:Boolean
+
+    init {
+        this.context=context
+        this.isChatChecked=isChatChecked
+        this.users=users
+    }
+    @SuppressLint("CutPasteId")
+    class ViewHolder(view: View):RecyclerView.ViewHolder(view){
+        lateinit var userNameText:TextView
+        lateinit var profileImageView:CircleImageView
+        lateinit var onlineText:ImageView
+        lateinit var offlineText:ImageView
+        lateinit var lastMessageText:TextView
+
+        init {
+            userNameText=view.findViewById(R.id.username)
+            profileImageView=view.findViewById(R.id.profile_image)
+            onlineText=view.findViewById(R.id.online_status)
+            offlineText=view.findViewById(R.id.offline_status)
+            lastMessageText=view.findViewById(R.id.last_message)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+                .inflate(R.layout.search_item_layout, parent, false)
+        return ViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = users[position]
+
+        holder.userNameText.text= item.getUsername()
+        Picasso.get().load(item.getProfile()).placeholder(R.drawable.ic_profile).into(holder.profileImageView)
+        
+    }
+
+    override fun getItemCount(): Int {
+        return users.size
+    }
 }
--- app/src/main/java/com/example/aaobaatkarain/Fragments/SearchFragment.kt
+++ app/src/main/java/com/example/aaobaatkarain/Fragments/SearchFragment.kt
@@ -5,7 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.aaobaatkarain.AdapterClasses.UserAdapter
+import com.example.aaobaatkarain.ModelClasses.Users
 import com.example.aaobaatkarain.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,16 +25,13 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var userAdapter:UserAdapter?=null
+    private var mUsers:List<Users>?=null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +39,36 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        val view:View=inflater.inflate(R.layout.fragment_search, container, false)
+        mUsers=ArrayList()
+        retrieveAllUsers()
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun retrieveAllUsers() {
+        val firebaseUserID=FirebaseAuth.getInstance().currentUser!!.uid
+        val refUsers= FirebaseDatabase.getInstance().reference.child("Users")
+
+        refUsers.addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                (mUsers as ArrayList<Users>).clear()
+                for(i in snapshot.children)
+                {
+                    val usr:Users?=snapshot.getValue(Users::class.java)
+                    if(usr!!.getUid() != firebaseUserID)
+                    {
+                        (mUsers as ArrayList<Users>).add(usr)
+                    }
                 }
+                userAdapter=UserAdapter(context!!,mUsers!!,false)
             }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
     }
+
 }
