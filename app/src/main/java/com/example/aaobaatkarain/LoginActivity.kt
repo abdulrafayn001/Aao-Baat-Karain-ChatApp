6ab5c26ec397f91df687f93fb6367935ebe88e83
Abdulrafay <abdulrafayn001@gmail.com>
2021-01-04 21:02:39 +0500

    Add comments to register and login files

--- app/src/main/java/com/example/aaobaatkarain/LoginActivity.kt
+++ app/src/main/java/com/example/aaobaatkarain/LoginActivity.kt
@@ -7,9 +7,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ProgressBar
 import android.widget.Toast
-
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -28,65 +26,84 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-
         Email = findViewById(R.id.email_login)
         Pswd = findViewById(R.id.password_login)
         Login_btn = findViewById(R.id.btn_login)
 
+        //Store instance of FirebaseAuth
         userAuth = FirebaseAuth.getInstance()
 
+        //Create a store with the name of "LOG" in shared preference
         val data: SharedPreferences = getSharedPreferences("LOG", MODE_PRIVATE)
         val gedit = data.edit()
+
+        //If user is already logged in go to main activity
         if(userAuth.currentUser != null && data.getBoolean("LogSucess",false))
         {
             startActivity(Intent(this,MainActivity::class.java))
             finish()
         }
 
+        //If the user click on login button bellow action will take place
         Login_btn.setOnClickListener {
+            //Convert email and password from raw to string
             val email:String = Email.text.toString()
             val pswd:String = Pswd.text.toString()
 
-            if (email == "")
-            {
-                Toast.makeText(this,"Email Can't be Empty", Toast.LENGTH_LONG).show()
-            }
-            else if (pswd == "")
-            {
-                Toast.makeText(this,"Password Can't be Empty", Toast.LENGTH_LONG).show()
-            }
-            else
-            {
-                var waitingBar: ProgressDialog = ProgressDialog(this)
-                waitingBar.setMessage("Signing in Please Wait!")
-                waitingBar.setProgressStyle(ProgressDialog.STYLE_SPINNER)
-                waitingBar.setTitle("Sign In")
-                waitingBar.show()
+
+            when {
+                //if email input field is empty
+                email == "" -> {
+                    Toast.makeText(this,"Email Can't be Empty", Toast.LENGTH_LONG).show()
+                }
+                //if password input filed is empty
+                pswd == "" -> {
+                    Toast.makeText(this,"Password Can't be Empty", Toast.LENGTH_LONG).show()
+                }
+                //if both email and password input fields are not empty
+                else -> {
+                    //Loading Screen
+                    val waitingBar: ProgressDialog = ProgressDialog(this)
+                    waitingBar.setMessage("Signing in Please Wait!")
+                    waitingBar.setProgressStyle(ProgressDialog.STYLE_SPINNER)
+                    waitingBar.setTitle("Sign In")
+                    waitingBar.show()
 
 
-                userAuth.signInWithEmailAndPassword(email,pswd)
-                        .addOnCompleteListener { task->
-                            if(task.isSuccessful)
-                            {
-                                waitingBar.dismiss()
-                                Toast.makeText(this,"Login Successfull", Toast.LENGTH_LONG).show()
-                                gedit.putBoolean("LogSucess",true)
-                                gedit.apply()
-                                gedit.commit()
-                                startActivity(Intent(this,MainActivity::class.java))
-                                finish()
+                    //Login User
+                    userAuth.signInWithEmailAndPassword(email,pswd)
+                            .addOnCompleteListener { task->
+                                if(task.isSuccessful)
+                                {
+                                    //dismiss the loading if successfully logged in
+                                    waitingBar.dismiss()
+
+                                    //Toast message on successful login
+                                    Toast.makeText(this,"Login Successful", Toast.LENGTH_LONG).show()
+                                    //Put a variable "LogSucess" with bool value "true" to make sure that if user again open the App he/she will go to main screen
+                                    gedit.putBoolean("LogSucess",true)
+                                    gedit.apply()
+                                    gedit.commit()
+
+
+                                    //After successfully login move to main activity
+                                    startActivity(Intent(this,MainActivity::class.java))
+                                    finish()
+                                }//If user do not login successfully then print a toast message
+                                else
+                                {
+                                    waitingBar.dismiss()
+                                    Toast.makeText(this,"Invalid Email or Password!", Toast.LENGTH_LONG).show()
+                                }
                             }
-                            else
-                            {
-                                waitingBar.dismiss()
-                                Toast.makeText(this,"Invalid Email or Password!", Toast.LENGTH_LONG).show()
-                            }
-                        }
+                }
             }
 
         }
 
     }
+
+    //If user click on signUp or register button then move to register activity
     fun userSignup(view: View) {
         startActivity(Intent(this, RegisterActivity::class.java))
         finish()
--- app/src/main/java/com/example/aaobaatkarain/RegisterActivity.kt
+++ app/src/main/java/com/example/aaobaatkarain/RegisterActivity.kt
@@ -14,7 +14,6 @@ import java.util.*
 import kotlin.collections.HashMap
 
 class RegisterActivity : AppCompatActivity() {
-
     private lateinit var userAuth:FirebaseAuth
     private lateinit var userRefernc:DatabaseReference
     private var FireBaseID:String = ""
@@ -36,8 +35,10 @@ class RegisterActivity : AppCompatActivity() {
         userAuth = FirebaseAuth.getInstance()
     }
 
+    //The bellow function will trigger when user click on SignUp or register button in RegisterActivity
     fun userSignup(view: View)
     {
+        //registerUser function will on clicking signUp button
         registerUser(
                 UserName.text.toString(),
                 Email.text.toString(),
@@ -47,42 +48,54 @@ class RegisterActivity : AppCompatActivity() {
     }
     private fun registerUser(user:String,email:String,pswd:String,c_pswd:String)
     {
-        if(user.equals(""))
+        //if username input filed is empty print an error message
+        if(user == "")
         {
             PrintError("UserName Can't be Empty")
         }
-        else if (email.equals(""))
+        //if email input filed is empty print an error message
+        else if (email == "")
         {
             PrintError("Email Can't be Empty")
         }
-        else if (pswd.equals(""))
+        //if password input filed is empty print an error message
+        else if (pswd == "")
         {
             PrintError("Password Can't be Empty")
         }
-        else if(c_pswd.equals(""))
+        //if confirm password input filed is empty print an error message
+        else if(c_pswd == "")
         {
             PrintError("Confirm Password Can't be Empty")
         }
-        else
+        else //if password and confirm password does not match print an error message
             if(c_pswd!=pswd)
             {
                 PrintError("Password does not match")
             }
         else
         {
+            //Check the validation of email and password
             if(validateEmail(email) && validatePassword(pswd))
             {
+                // Loading screen while register
                 val waitingBar: ProgressDialog = ProgressDialog(this)
                 waitingBar.setMessage("Registering your account. Please Wait!")
                 waitingBar.setProgressStyle(ProgressDialog.STYLE_SPINNER)
                 waitingBar.setTitle("Register")
                 waitingBar.show()
+                //Create a user with given email and password
                 userAuth.createUserWithEmailAndPassword(email, pswd).addOnCompleteListener { task ->
+                    //if user account is successfully created
                     if (task.isSuccessful)
                     {
+                        //Get a unique id of newly created account
                         FireBaseID = userAuth.currentUser!!.uid
+
+                        //Get the reference of newly created user fro firebase database
                         userRefernc = FirebaseDatabase.getInstance().reference.child("Users").child(FireBaseID)
 
+                        //Create a HashMap and store all the information of user in it.
                         val UserMap = HashMap<String, Any>()
                         UserMap["uid"] = FireBaseID
                         UserMap["username"] = user
@@ -93,14 +106,17 @@ class RegisterActivity : AppCompatActivity() {
                         UserMap["website"] = "www.google.com"
                         UserMap["status"] = "offline"
                         UserMap["search"] = user.toLowerCase(Locale.ROOT)
+
+                        //Update the data of newly created account in firebase database
                         userRefernc.updateChildren(UserMap).addOnCompleteListener { task ->
+                            //If data is successfully stored in database, stop loading.
                             if (task.isSuccessful)
                             {
                                 waitingBar.dismiss()
                                 PrintError("Registered Successfully!")
                                 startActivity(Intent(this, LoginActivity::class.java))
                                 finish()
-                            }
+                            }//if data is not successfully stored in data base print ab error message.
                             else
                             {
                                 waitingBar.dismiss()
@@ -121,33 +137,34 @@ class RegisterActivity : AppCompatActivity() {
 
         }
     }
+
+    
     private fun validateEmail(email:String): Boolean {
 
-        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        return if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
         {
-                PrintError("Invalid Email Address")
-                return false
+            PrintError("Invalid Email Address")
+            false
         }
         else
         {
-                return true
+            true
         }
     }
     private fun validatePassword(password: String): Boolean
     {
-        if(password.contains(" "))
-        {
-            PrintError("Password Should not contain spaces. Try Again!")
-            return false
-        }
-        else if(password.length < 8)
-        {
-            PrintError("Password Should have minimum length of 8 Characters. Try Again!")
-            return false
-        }
-        else
-        {
-           return true
+        return when {
+            password.contains(" ") -> {
+                PrintError("Password Should not contain spaces. Try Again!")
+                false
+            }
+            password.length < 8 -> {
+                PrintError("Password Should have minimum length of 8 Characters. Try Again!")
+                false
+            }
+            else -> {
+                true
+            }
         }
     }
     private fun PrintError(v:String)
