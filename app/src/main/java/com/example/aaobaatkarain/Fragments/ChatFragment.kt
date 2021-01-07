4df0b261f5fd243c6fccb734d164a068f932792d
Moiz Asad <moizasad86@gmail.com>
2021-01-07 11:52:26 +0500

    Chat list fetching from database

--- app/src/main/java/com/example/aaobaatkarain/Fragments/ChatFragment.kt
+++ app/src/main/java/com/example/aaobaatkarain/Fragments/ChatFragment.kt
@@ -5,56 +5,89 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.aaobaatkarain.AdapterClasses.UserAdapter
+import com.example.aaobaatkarain.ModelClasses.Chatlist
+import com.example.aaobaatkarain.ModelClasses.Users
 import com.example.aaobaatkarain.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ChatFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ChatFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var userAdapter:UserAdapter? = null
+    private var mUsers: List<Users>? = null
+    private var chatUsersList: List<Chatlist>? = null
+    lateinit var recycView:RecyclerView
+    private var firebaseUser: FirebaseUser? = null
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
-    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+    {
+        val viewRef:View = inflater.inflate(R.layout.fragment_chat, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChatFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChatFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        recycView = viewRef.findViewById(R.id.chatList)
+        recycView.setHasFixedSize(true)
+        recycView.layoutManager = LinearLayoutManager(context)
+
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+
+        chatUsersList = ArrayList()
+
+        val refUser = FirebaseDatabase.getInstance().reference.child("ChatLists").child(firebaseUser!!.uid)
+        refUser.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                (chatUsersList as ArrayList).clear()
+
+                for (dataOnj in snapshot.children) {
+
+                    val chatLst = dataOnj.getValue(Chatlist::class.java)
+
+                    (chatUsersList as ArrayList).add(chatLst!!)
                 }
+                reteiveChatList()
             }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+
+        return viewRef
+
+    }
+
+    private fun reteiveChatList()
+    {
+        mUsers = ArrayList()
+        val refUser = FirebaseDatabase.getInstance().reference.child("Users")
+        refUser.addValueEventListener(object : ValueEventListener
+        {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                (mUsers as ArrayList).clear()
+
+                for (dataPack in snapshot.children)
+                {
+                    val userRef = dataPack.getValue(Users::class.java)
+                    for( chatItem in chatUsersList!!)
+                    {
+                        if(userRef!!.getUid().equals(chatItem.getId()))
+                        {
+                            (mUsers as ArrayList).add(userRef)
+                        }
+                    }
+                }
+                userAdapter = UserAdapter(context!!,(mUsers as ArrayList<Users>),true)
+                recycView.adapter = userAdapter
+            }
+            override fun onCancelled(error: DatabaseError) {}
+        })
     }
 }
+
+
+
+
