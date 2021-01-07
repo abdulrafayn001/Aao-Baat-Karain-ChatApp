599bfa27a93bab6bef240e77c3d0e3fe8a43019d
Moiz Asad <moizasad86@gmail.com>
2021-01-07 14:58:11 +0500

    Splash Screen for application added

---
+++ app/src/main/java/com/example/aaobaatkarain/SplahScreen.kt
@@ -0,0 +1,40 @@
+package com.example.aaobaatkarain
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+
+
+
+class SplahScreen : AppCompatActivity() {
+
+    lateinit var TextSplash:TextView
+    lateinit var Logo:ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splah_screen)
+
+        TextSplash = findViewById(R.id.nameSplash)
+        Logo = findViewById(R.id.logosplash)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.move)
+        animation.duration = 2000
+        val animation2 = AnimationUtils.loadAnimation(this, R.anim.move_opos)
+        animation2.duration = 2000
+
+        TextSplash.startAnimation(animation)
+        Logo.startAnimation(animation2)
+
+        Handler().postDelayed({
+            finish() }, 4000)
+
+
+
+    }
+}
